aefb08bc55d45dd9ce888333ff81e66738569f2a
Midhun P J <Midhun.Jayan@grit-stone.com>
2021-09-04 22:17:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+import * as MovieList from '../public/movie-list.json';
+
 function App() {
+  const [moveis, setMovies] = useState(MovieList);
   return (
     <div>
       It works
