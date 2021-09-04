45e4079bbd376e72a218d11fb25b2d64caeac9b1
Midhun P J <Midhun.Jayan@grit-stone.com>
2021-09-04 22:26:45 +0530

    sample listing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { useState } from 'react';
-import * as MovieList from '../public/movie-list.json';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [moveis, setMovies] = useState(MovieList);
-  return (
-    <div>
-      It works
-    </div>
-  );
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    fetch('./movie-list.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
+  return <div>
+    <ul>
+      {
+        movies?.slice(0, 20).map((m, index)=>{
+          return (
+            <li key={index}>
+              <span>{m.title}</span>
+              <img src={m.poster} alt="poster"/>
+            </li>
+          )
+        })
+      }
+    </ul>
+  </div>;
 }
 
 export default App;
