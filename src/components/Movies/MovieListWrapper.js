674e440c24e4d8b6fa9dbf39e50872fa37d7ec5b
Midhun P J <Midhun.Jayan@grit-stone.com>
2021-09-07 13:41:36 +0530

    slider ui completed

--- src/components/Movies/MovieListWrapper.js
+++ src/components/Movies/MovieListWrapper.js
@@ -1,27 +1,48 @@
-import classes from './MovieListWrapper.module.scss';
+import { useRef, useState, useEffect } from 'react';
+
 import SlideButton from '../Slider/SlideButton';
+import classes from './MovieListWrapper.module.scss';
 
 function MovieLisWrapper(props) {
-  console.log(props);
-  const handlePrev = () => {
-    console.log('previous clicked');
-  };
+  const listRef = useRef();
 
-  const handleNext = () => {
-    console.log('next clicked');
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [hasNext, setHasNext] = useState(true);
+  const [hasPrevious, setHasPrevious] = useState(false);
+  const totalItems = props?.movieList?.length;
+  useEffect(() => {
+    if (slideNumber === 0) {
+      setHasPrevious(false);
+    }
+  }, [slideNumber]);
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 80;
+    const itemsPerPage = Math.ceil(listRef.current.getBoundingClientRect().width / 130);
+    const containerWidth = listRef.current.getBoundingClientRect().width - 180;
+    const totalSlides = Math.ceil(totalItems / itemsPerPage);
+    if (direction === 'prev' && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${containerWidth + distance}px)`;
+    }
+    if (direction === 'next' && slideNumber < totalSlides) {
+      setSlideNumber(slideNumber + 1);
+      setHasPrevious(true);
+      listRef.current.style.transform = `translateX(${-containerWidth + distance}px)`;
+    }
   };
   return (
     <div className={classes.wrapper}>
       <span className={classes.rowHeaderTitle}>{props.title}</span>
-      <div className={classes?.container}>
+      {hasPrevious && <SlideButton onClick={() => handleClick('prev')} type="prev" />}
+      {hasNext && <SlideButton onClick={() => handleClick('next')} type="next" />}
+      <div className={classes?.container} ref={listRef}>
         {props.movieList.map((mv) => {
-          return <div className={classes.item} key={mv.id}>
-            <img src={mv.poster} alt={mv.title}/>
-          </div>;
+          return (
+            <div className={classes.item} key={mv.id}>
+              <img src={mv.poster} alt={mv.title} />
+            </div>
+          );
         })}
-
-        <SlideButton onClick={handlePrev} type="prev" />
-        <SlideButton onClick={handleNext} type="next" />
       </div>
     </div>
   );
